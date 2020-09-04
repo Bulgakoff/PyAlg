bdcbf046fa9ce95736ba92f17b5149ea7b59840e
Alex <hlbu@yandex.ru>
2020-09-04 23:52:03 +0300

    Lesson08/2

--- Lesson08/task_1.py
+++ Lesson08/task_1.py
@@ -3,12 +3,12 @@ import hashlib
 
 def h_papa(s):
     h_s = set()
-    sub_s = set()
+    # sub_s = set()
     for i in range(len(s)):
         for j in range(len(s) + 1):
             if len(s[i:j]) != len(s) and s[i:j] != '':
                 h_s.add(hashlib.sha1(s[i:j].encode('utf-8')).hexdigest())
-                sub_s.add(s[i:j])
+                # sub_s.add(s[i:j])
 
     return f'количество подстрок в строке \'{s}\' = {len(h_s)}'
 
